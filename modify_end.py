02add9c6587adb6a4e9b7969732be4dde08a2c96
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2015-05-02 13:53:31 +0900

    birth of modify_end

---
+++ modify_end.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import json
+import webapp2
+import jinja2
+import logging
+from google.appengine.ext import ndb
+from checker import EndPaper
+from main import BaseHandler
+
+class ModifyEndHandler(BaseHandler):
+    def get(self):
+        self.horizontal_line('EndPapers')
+        q = ndb.gql("SELECT * FROM EndPaper")
+        for i in q.iter():
+            self.response.write(i)
+            self.response.write('<br>')
+        self.render('modify_end.html')
+    def post(self):
+        self.horizontal_line('EndPapers')
+        q = ndb.gql("SELECT * FROM EndPaper WHERE area = :1", self.request.get('area'))
+        end_paper = q.get()
+        if end_paper is None:
+            self.redirect('/utils/disp')
+        end_paper.number = self.request.get('number')
+        end_paper.put()
+        self.redirect('/utils/disp')
+
+    def horizontal_line(self, name):
+        self.response.write('---------------------------<br>')
+        self.response.write(name + '<br>')
+        self.response.write('---------------------------<br>')
+
+app = webapp2.WSGIApplication([
+    ('/utils/modify_end', ModifyEndHandler),
+], debug=True)
