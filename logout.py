97f338a72e00dcb786c0a3c56083613c9006d49c
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2015-05-03 10:33:15 +0900

    modify logout setting

--- logout.py
+++ logout.py
@@ -10,7 +10,6 @@ class LogoutHandler(webapp2.RequestHandler):
     def get(self):
         if users.get_current_user() is not None:
             self.redirect(users.create_logout_url(self.request.uri))
-        #self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/utils/logout', LogoutHandler),
