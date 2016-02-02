b10ea6942baad8711509fed3e0a63bbba8dc0daf
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2016-02-02 13:23:19 +0900

    fix

--- settings.py
+++ settings.py
@@ -9,9 +9,4 @@ def filter_user(user, obj):
     if user is None:
         obj.redirect(users.create_login_url(obj.request.uri))
         return True
-    if not '@XXXXXXXXX' in user.email():
-        obj.response.write('Access denied.')
-        obj.response.write('<br>')
-        obj.response.write('<a href="' + users.create_logout_url(obj.request.uri) + '">Logout</a>')
-        return True
     return False
